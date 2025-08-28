9ed8e74f0967c2692569df93c7bfa5d8f49d2547
Yurieb <B00136730@mytudublin.ie>
2025-08-28 08:05:00 +0100

    small change

--- src/app/api/products/route.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "@/lib/mongodb";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const db = await connect();
-  const products = await db.collection("products").find().toArray();
-  return NextResponse.json(products);
-}
